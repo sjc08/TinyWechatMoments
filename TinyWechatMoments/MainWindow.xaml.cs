73f6117a1227439d8c6b034a5d8659e470a5ee48
AlexSJC <cn_sjc@outlook.com>
2024-08-31 14:27:50 +0800

    feat: update

--- TinyWechatMoments/MainWindow.xaml.cs
+++ TinyWechatMoments/MainWindow.xaml.cs
@@ -8,10 +8,17 @@ namespace TinyWechatMoments
     /// </summary>
     public partial class MainWindow : GlowWindow
     {
+        private Data data = JsonConfig.Load<Data>();
+
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = JsonConfig.Load<Data>();
+            DataContext = data;
+        }
+
+        private void AddComment(object parameter)
+        {
+
         }
     }
 }
