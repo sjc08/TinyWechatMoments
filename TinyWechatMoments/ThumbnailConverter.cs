1becaff93775d325616065e9ddb136934f5a1367
AlexSJC <cn_sjc@outlook.com>
2024-09-07 22:44:40 +0800

    feat: optimize

--- TinyWechatMoments/ThumbnailConverter.cs
+++ TinyWechatMoments/ThumbnailConverter.cs
@@ -1,23 +1,26 @@
-﻿using Asjc.ThumbnailProvider;
-using Asjc.Utils;
+﻿using Asjc.Utils;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Interop;
 using System.Windows.Media.Imaging;
+using static Asjc.ThumbnailProvider.ThumbnailProvider;
 
 namespace TinyWechatMoments
 {
     public class ThumbnailConverter : IValueConverter
     {
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var bitmap = Tryer.Try(() => ThumbnailProvider.GetThumbnail(value.ToString()));
-            return bitmap is null ? null : Imaging.CreateBitmapSourceFromHBitmap(
-                bitmap.GetHbitmap(),
+            var hBitmap = GetHThumbnail(value.ToString());
+            var source = Tryer.Try(() => Imaging.CreateBitmapSourceFromHBitmap(
+                hBitmap,
                 IntPtr.Zero,
                 Int32Rect.Empty,
-                BitmapSizeOptions.FromEmptyOptions());
+                BitmapSizeOptions.FromEmptyOptions()));
+            DeleteObject(hBitmap);
+            return source;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
